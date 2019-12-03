1f2348afcba4bbd46562faad209f66539e0a71aa
Salsa85 <info@zencule.com>
2019-12-03 18:54:05 +0100

    bug fixes

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,7 @@
 |
 */
 
-Auth::routes([
-  'register' => false, // Registration Routes...
-  'reset' => false, // Password Reset Routes...
-  'verify' => false, // Email Verification Routes...
-]);
+Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('dashboard');
 Route::get('/errors', 'AddError@index')->name('error');
